ff1a9f439b8907c5e1fcf92bf7dd17b67d5ab0ce
Zied Ben Haj Salah <ziedhajsalah@gmail.com>
2016-10-20 23:35:07 +0100

    initial commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,47 @@
+'use strict'
+
+import gulp from 'gulp'
+import path from 'path'
+import sync from 'run-sequence'
+import browserSync from 'browser-sync'
+
+let reload = () => browserSync.reload()
+let root = 'client'
+
+// helper method for resolving paths
+let resolveToApp = (glob) => {
+  glob = glob || ''
+  return path.join(root, 'app', glob) // app/{glob}
+}
+
+// map of all paths
+let paths = {
+  js: resolveToApp('**/*!(.spec.js).js'), // exclude spec files
+  styl: resolveToApp('**/*.styl'), // stylesheets
+  html: [
+    resolveToApp('**/*.html'),
+    path.join(root, 'index.html')
+  ],
+  entry: path.join(root, 'app/app.js'),
+  output: root
+}
+
+gulp.task('reload', done => {
+  reload()
+  done()
+})
+
+gulp.task('serve', () => {
+  browserSync({
+    port: process.env.PORT || 3000,
+    open: false,
+    server: { baseDir: root }
+  })
+})
+
+gulp.task('watch', ['serve'], () => {
+  let allPaths = [].concat([paths.js], paths.html, [paths.styl])
+  gulp.watch(allPaths, ['reload'])
+})
+
+gulp.task('default', ['watch'])
